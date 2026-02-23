89008f5d14367f65947cf87b8d421d1d65394dae
Giovanna <giovanna.aalves12@gmail.com>
2026-02-23 11:05:46 -0300

    first commit

---
+++ Aula03/exercicios.js
@@ -0,0 +1,56 @@
+// Exercício 1
+let cores = ["azul" , "rosa" , "verde"];
+console.log (cores[1]);
+
+// Exercício 2
+let numeros = [10, 20, 30, 40, 50];
+console.log(numeros[0]);
+console.log(numeros[numeros.length - 1]);
+
+// Exercício 3
+let frutas = ["maçã", "banana"];
+frutas.push("uva");
+console.log(frutas);
+console.table (frutas);
+
+// Exercício 4
+let nomes = ["Giovanna", "Felipe", "Benício", "Jade"];
+nomes.pop();
+console.log(nomes);
+console.table (nomes);
+
+// Exercício 5
+let times = ["São Paulo" , "Flamengo", "Vasco" ];
+console.log(times);
+console.table (times);
+
+// Exercício 6
+let animais = ["cachorro", "gato", "coelho"];
+console.log(animais);
+console.table (animais);
+
+// Exercício 7
+let carros = ["Ford", "Chevrolet", "Toyota"];
+carros[1] = "Honda";
+console.log(carros);
+console.table (carros);
+
+// Exercício 8
+let linguagens = ["Python", "Java", "JavaScript"];
+console.log( linguagens ("JavaScript"));
+console.table (linguagens);
+
+
+// Exercício 9
+let valores = [5, 10, 15];
+let soma = valores[0] + valores[1] + valores[2];
+console.log(soma);
+
+// Exercício 10
+let tarefas = [];
+tarefas.push("Estudar", "Treinar", "Ler");
+console.log("1 - " + tarefas[0]);
+console.log("2 - " + tarefas[1]);
+console.log("3 - " + tarefas[2]);
+
+
---
+++ Aula03/tipos-de-dados.js
@@ -0,0 +1,30 @@
+// Um array é uma lista que pode armazenar vários valores
+
+//let frutas = ["Maçã" , "Banana" , "Uva"];
+
+//console.log(frutas[2]);
+//console.table(frutas);
+
+//adiciona ao final do array
+//frutas.push("Morango");
+//console.log(frutas);
+//console.table(frutas);
+
+//remove a última posição
+
+
+//remove da posição indicada
+//frutas.splice(1);
+//console.table(frutas);
+
+//Crie um array chamado animais com 03 animais e adicione mais tres animais
+
+let animais = ["Gato" , "Cachorro" , "Cavalo"];
+
+console.log(animais);
+console.table(animais)
+
+animais.push("Sapo" , "Iguana" ,"Peixe")
+console.log(animais);
+console.table(animais);
+
